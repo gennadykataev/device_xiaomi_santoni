6ebd2617d2a6cbb1475d84b9a6d68dd24008508e
gennadykataev <gennady.kataev@protonmail.com>
2021-10-26 11:47:19 +0300

    santoni: cleanup arrow and octavi

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,3 @@
-# ArrowOS
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
-
-COMMON_LUNCH_CHOICES := \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
-
-# AEX
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/aosp_santoni.mk
 
@@ -15,12 +5,3 @@ COMMON_LUNCH_CHOICES += \
     aosp_santoni-userdebug \
     aosp_santoni-user \
     aosp_santoni-eng
-
-# Octavi-OS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/octavi_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    octavi_santoni-userdebug \
-    octavi_santoni-user \
-    octavi_santoni-eng
